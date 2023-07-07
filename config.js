e9d143622d178b3a22a984cf60997dcc60bd7a6c
tharu0428 <136889110+tharu0428@users.noreply.github.com>
2023-07-07 09:26:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94771649172', 'me', true],
+  ['94771649172', 'Sl Tharu', true], 
   [''] 
 ] //Number of owners
 
